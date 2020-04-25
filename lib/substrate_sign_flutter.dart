22df59adbf37459663b6338fd72c4c55344265d6
Hanwen Cheng <heawen.cheng@gmail.com>
2020-04-25 18:10:23 +0200

    add reference related functions

--- lib/substrate_sign_flutter.dart
+++ lib/substrate_sign_flutter.dart
@@ -27,14 +27,35 @@ import 'package:ffi/ffi.dart';
 // Typedef's
 ///////////////////////////////////////////////////////////////////////////////
 
-typedef RustGreetingFunc = Pointer<Utf8> Function(Pointer<Utf8>);
-typedef RustGreetingFuncNative = Pointer<Utf8> Function(Pointer<Utf8>);
-
 typedef FreeStringFunc = void Function(Pointer<Utf8>);
 typedef FreeStringFuncNative = Void Function(Pointer<Utf8>);
 
-typedef RustRandomPhraseFunction = Pointer<Utf8> Function(int);
-typedef RustRandomPhraseFunctionNative = Pointer<Utf8> Function(Uint32);
+typedef RustRandomPhrase = Pointer<Utf8> Function(int);
+typedef RustRandomPhraseNative = Pointer<Utf8> Function(Uint32);
+
+typedef RustSign = Pointer<Utf8> Function(Pointer<Utf8>, Pointer<Utf8>);
+typedef RustSignNative = Pointer<Utf8> Function(Pointer<Utf8>, Pointer<Utf8>);
+
+typedef RustSubstrateAddress = Pointer<Utf8> Function(Pointer<Utf8>, int);
+typedef RustSubstrateAddressNative = Pointer<Utf8> Function(Pointer<Utf8>, Uint32);
+
+typedef RustEncrypt = Pointer<Utf8> Function(Pointer<Utf8>, Pointer<Utf8>);
+typedef RustEncryptNative = Pointer<Utf8> Function(Pointer<Utf8>, Pointer<Utf8>);
+
+typedef RustDecrypt = Pointer<Utf8> Function(Pointer<Utf8>, Pointer<Utf8>);
+typedef RustDecryptNative = Pointer<Utf8> Function(Pointer<Utf8>, Pointer<Utf8>);
+
+typedef RustDecryptWithRef = int Function(Pointer<Utf8>, Pointer<Utf8>);
+typedef RustDecryptWithRefNative = Int64 Function(Pointer<Utf8>, Pointer<Utf8>);
+
+typedef RustDestroyDataRef = void Function(int);
+typedef RustDestroyDataRefNative = Void Function(Int64);
+
+typedef RustSignWithRef = Pointer<Utf8> Function(int, Pointer<Utf8>, Pointer<Utf8>);
+typedef RustSignWithRefNative = Pointer<Utf8> Function(Int64, Pointer<Utf8>, Pointer<Utf8>);
+
+typedef RustSubstrateAddressWithRef = Pointer<Utf8> Function(int, Pointer<Utf8>, int);
+typedef RustSubstrateAddressWithRefNative = Pointer<Utf8> Function(Int64, Pointer<Utf8>, Uint32);
 
 ///////////////////////////////////////////////////////////////////////////////
 // Load the library
@@ -48,45 +69,49 @@ final DynamicLibrary nativeSubstrateSignLib = Platform.isAndroid
 // Locate the symbols we want to use
 ///////////////////////////////////////////////////////////////////////////////
 
-final RustGreetingFunc rustGreeting = nativeSubstrateSignLib
-    .lookup<NativeFunction<RustGreetingFuncNative>>("rust_greeting")
-    .asFunction();
-
 final FreeStringFunc freeCString = nativeSubstrateSignLib
     .lookup<NativeFunction<FreeStringFuncNative>>("rust_cstr_free")
     .asFunction();
 
-final RustRandomPhraseFunction rustRandomPhrase = nativeSubstrateSignLib
-    .lookup<NativeFunction<RustRandomPhraseFunctionNative>>("random_phrase")
+final RustRandomPhrase rustRandomPhrase = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustRandomPhraseNative>>("random_phrase")
+    .asFunction();
+
+final RustSign rustSign = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustSignNative>>("substrate_sign")
+    .asFunction();
+
+final RustSubstrateAddress rustSubstrateAddress = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustSubstrateAddressNative>>("substrate_address")
+    .asFunction();
+
+final RustEncrypt rustEncrypt = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustEncryptNative>>("encrypt_data")
+    .asFunction();
+
+final RustDecrypt rustDecrypt = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustDecryptNative>>("decrypt_data")
+    .asFunction();
+
+final RustDecryptWithRef rustDecryptWithRef = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustDecryptWithRefNative>>("decrypt_data_with_ref")
+    .asFunction();
+
+final RustDestroyDataRef rustDestroyDataRef = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustDestroyDataRefNative>>("destroy_data_ref")
+    .asFunction();
+
+final RustSignWithRef rustSignWithRef = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustSignWithRefNative>>("substrate_sign_with_ref")
+    .asFunction();
+
+final RustSubstrateAddressWithRef rustSubstrateAddressWithRef = nativeSubstrateSignLib
+    .lookup<NativeFunction<RustSubstrateAddressWithRefNative>>("substrate_address_with_ref")
     .asFunction();
 
 ///////////////////////////////////////////////////////////////////////////////
 // HANDLERS
 ///////////////////////////////////////////////////////////////////////////////
-
-String nativeGreeting(String name) {
-  if (nativeSubstrateSignLib == null)
-    return "ERROR: The library is not initialized 🙁";
-
-  print("  ${nativeSubstrateSignLib.toString()}"); // Instance info
-
-  final argName = Utf8.toUtf8(name);
-  print("- Calling rust_greeting with argument:  $argName");
-
-  // The actual native call
-  final resultPointer = rustGreeting(argName);
-  print("- Result pointer:  $resultPointer");
-
-  final greetingStr = Utf8.fromUtf8(resultPointer);
-  print("- Response string:  $greetingStr");
-
-  // Free the string pointer, as we already have
-  // an owned String to return
-  print("- Freing the native char*");
-  freeCString(resultPointer);
-
-  return greetingStr;
-}
 
 String randomPhrase(int digits){
   if (nativeSubstrateSignLib == null)
@@ -96,4 +121,68 @@ String randomPhrase(int digits){
   final phrasePointer = rustRandomPhrase(digits);
   return Utf8.fromUtf8(phrasePointer);
 }
+
+String substrateSign(String suri, String message) {
+  final utf8Suri =  Utf8.toUtf8("$suri//path///");
+  final utf8Message = Utf8.toUtf8(message);
+  final utf8SignedMessage = rustSign(utf8Suri, utf8Message);
+  final signedMessage = Utf8.fromUtf8(utf8SignedMessage);
+  freeCString(utf8SignedMessage);
+  return signedMessage;
+}
+
+String substrateAddress (String seed, int prefix) {
+  final utf8Seed = Utf8.toUtf8(seed);
+  final utf8Address = rustSubstrateAddress(utf8Seed, prefix);
+  final address = Utf8.fromUtf8(utf8Address);
+  freeCString(utf8Address);
+  return address;
+}
+
+String encryptData(String data, String password) {
+  final utf8Data = Utf8.toUtf8(data);
+  final utf8Password = Utf8.toUtf8(password);
+  final utf8Encrypted = rustEncrypt(utf8Data, utf8Password);
+  final encrypted = Utf8.fromUtf8(utf8Encrypted);
+  freeCString(utf8Encrypted);
+  return encrypted;
+}
+
+String decryptData(String data, String password) {
+  final utf8Data = Utf8.toUtf8(data);
+  final utf8Password = Utf8.toUtf8(password);
+  final utf8Decrypted = rustDecrypt(utf8Data, utf8Password);
+  final decrypted = Utf8.fromUtf8(utf8Decrypted);
+  freeCString(utf8Decrypted);
+  return decrypted;
+}
+
+int decryptDataWithRef(String data, String password) {
+  final utf8Data = Utf8.toUtf8(data);
+  final utf8Password = Utf8.toUtf8(password);
+  final ref = rustDecryptWithRef(utf8Data, utf8Password);
+  return ref;
+}
+
+void destroyDataRef(int ref){
+  rustDestroyDataRef(ref);
+}
+
+String substrateSignWithRef(int seedRef, String suriSuffix, String message) {
+  final utf8SuriSuffix =  Utf8.toUtf8("$suriSuffix//path///");
+  final utf8Message = Utf8.toUtf8(message);
+  final utf8SignedMessage = rustSignWithRef(seedRef, utf8SuriSuffix, utf8Message);
+  final signedMessage = Utf8.fromUtf8(utf8SignedMessage);
+  freeCString(utf8SignedMessage);
+  return signedMessage;
+}
+
+String substrateAddressWithRef(int seedRef, String suriSuffix, int prefix) {
+  final utf8SuriSuffix = Utf8.toUtf8(suriSuffix);
+  final utf8Address = rustSubstrateAddressWithRef(seedRef, utf8SuriSuffix, prefix);
+  final address = Utf8.fromUtf8(utf8Address);
+  freeCString(utf8Address);
+  return address;
+}
+
 
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,7 +11,7 @@ const CRYPTO_ITERATIONS: u32 = 10240;
 fn get_str(rust_ptr: *const c_char) -> String {
     let c_str = unsafe { CStr::from_ptr(rust_ptr) };
     let result_string = match c_str.to_str() {
-        Err(_) => "",
+        Err(_) => "input string error",
         Ok(string) => string,
     };
     return String::from(result_string)
@@ -19,11 +19,6 @@ fn get_str(rust_ptr: *const c_char) -> String {
 
 fn get_ptr(rust_string: &str) -> *mut c_char {
     CString::new(rust_string).unwrap().into_raw()
-}
-
-#[no_mangle]
-pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
-    get_ptr(&get_str(to))
 }
 
 #[no_mangle]
@@ -114,12 +109,13 @@ pub extern fn substrate_sign(
     let keypair_option = sr25519::KeyPair::from_suri(&get_str(suri));
     let keypair = match keypair_option {
         Some(c) => c,
-        _ => return get_ptr("")
+        _ => return get_ptr("keypair generation error")
     };
-    let message_result: Result<Vec<u8>, FromHexError> = (&get_str(text)).from_hex();
+    let text_string = get_str(text);
+    let message_result: Result<Vec<u8>, FromHexError> = text_string.from_hex();
     let message: Vec<u8> = match message_result {
         Ok(c) => c,
-        _ => return get_ptr("")
+        _ => return get_ptr(&get_str(text))
     };
     let signature = keypair.sign(&message);
     let result: String = signature.to_hex();
@@ -151,4 +147,77 @@ pub extern fn schnorrkel_verify(
         Some(result)=> result,
         _ => false
     }
+}
+
+
+#[no_mangle]
+pub extern fn decrypt_data_with_ref(
+    data: *const c_char,
+    password: *const c_char,
+) -> i64 {
+    let password = Protected::new(get_str(password).into_bytes());
+
+    let result_crypto = serde_json::from_str(&get_str(data));
+    let crypto: Crypto = match result_crypto {
+        Ok(c) => c,
+        _ => return 0
+    };
+    let decrypted_result = crypto.decrypt(&password);
+    let decrypted = match decrypted_result {
+        Ok(c) => c,
+        _ => return 0
+    };
+
+    let decrypted_ptr = Box::into_raw(Box::new(decrypted)) as i64;
+    return decrypted_ptr;
+}
+
+#[no_mangle]
+pub extern fn substrate_sign_with_ref(
+    seed_ref: i64,
+    suri_suffix: *const c_char,
+    text: *const c_char
+) -> *mut c_char {
+    let seed = unsafe { Box::from_raw(seed_ref  as *mut String) };
+    let suri_suffix_str = get_str(suri_suffix);
+    let suri = format!("{}{}", &seed, suri_suffix_str);
+    let keypair_option = sr25519::KeyPair::from_suri(&suri);
+    let keypair = match keypair_option {
+        Some(c) => c,
+        _ => return get_ptr("keypair generation error")
+    };
+    let text_string = get_str(text);
+    let message_result: Result<Vec<u8>, FromHexError> = text_string.from_hex();
+    let message: Vec<u8> = match message_result {
+        Ok(c) => c,
+        _ => return get_ptr(&get_str(text))
+    };
+    let signature = keypair.sign(&message);
+    let result: String = signature.to_hex();
+    let _ = Box::into_raw(seed) as i64;
+    CString::new(result).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern fn substrate_address_with_ref(
+    seed_ref: i64,
+    suri_suffix: *const c_char,
+    prefix: u8
+) -> *mut c_char {
+    let seed = unsafe { Box::from_raw(seed_ref  as *mut String) };
+    let suri_suffix_str = get_str(suri_suffix);
+    let suri = format!("{}{}", &seed, suri_suffix_str);
+    let keypair_option = sr25519::KeyPair::from_suri(&suri);
+    let keypair = match keypair_option {
+        Some(c) => c,
+        _ => return get_ptr("")
+    };
+    let _ = Box::into_raw(seed) as i64;
+    let rust_string = keypair.ss58_address(prefix);
+    get_ptr(&rust_string)
+}
+
+#[no_mangle]
+pub extern fn destroy_data_ref(data_ref: i64) -> () {
+    unsafe { Box::from_raw(data_ref as *mut String) };
 }
